195b938551adf6ffe9ad1646a785d1101cf2c724
wangguibao <wangguibao@baidu.com>
2019-04-09 19:39:07 +0800

    Remove unused scripts
    Change-Id: I661d35be5ad6b6aadd679ccb8faf72d049b94460

--- predictor/scripts/start.sh
+++
@@ -1,1 +0,0 @@
-TCMALLOC_SAMPLE_PARAMETER=524288 /home/work/paddle-serving/bin/pdserving --workflow_path=/home/work/paddle-serving/conf/ --inferservice_path=/home/work/paddle-serving/conf/ --logger_path=/home/work/paddle-serving/conf/ --resource_path=/home/work/paddle-serving/conf/
