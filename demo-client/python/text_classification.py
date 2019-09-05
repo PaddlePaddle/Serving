b3113293bab80ad8fb2c117cff50d6832805474a
wangguibao <wangguibao@baidu.com>
2019-09-05 16:36:54 +0800

    Demo: HTTP interface via Python and PHP

--- demo-client/python/text_classification.py
+++ demo-client/python/text_classification.py
@@ -1,4 +1,17 @@
-# -*- coding: utf-8 -*-
+#   Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 import httplib
 import sys
@@ -38,7 +51,6 @@ if __name__ == "__main__":
     ret = data_reader(sys.argv[1], samples, labels)
 
     conn = httplib.HTTPConnection("127.0.0.1", 8010)
-    # conn.putheader('Content-Type', 'application/json') # 如果server版本在r31987后，不需要设置这个。
 
     for i in range(0, len(samples) - BATCH_SIZE, BATCH_SIZE):
         batch = samples[i:i + BATCH_SIZE]
