d465b2fecb248b868cf80f6be7dc3aa8b6cc98c8
guru4elephant <guru4elephant@gmail.com>
2020-02-05 12:34:02 +0800

    add imdb code

--- python/examples/imdb/test_client.py
+++ python/examples/imdb/test_client.py
@@ -1,4 +1,4 @@
-from paddle_serving.serving_client import Client
+from paddle_serving_client import Client
 import sys
 
 client = Client()
@@ -7,10 +7,11 @@ client.connect(["127.0.0.1:9292"])
 
 for line in sys.stdin:
     group = line.strip().split()
-    words = [int(x) for x in group[1:int(group[0])]]
+    words = [int(x) for x in group[1:int(group[0]) + 1]]
     label = [int(group[-1])]
     feed = {"words": words, "label": label}
-    fetch = ["acc", "cost", "prediction"]
+    fetch = ["cost", "acc", "prediction"]
     fetch_map = client.predict(feed=feed, fetch=fetch)
-    print("{} {}".format(fetch_map["prediction"][1], label[0]))
+    print(fetch_map)
+    #print("{} {}".format(fetch_map["prediction"][1], label[0]))
     
