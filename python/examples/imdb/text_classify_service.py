d570db840953c66173ed79f04b870ef11bb0cead
guru4elephant <guru4elephant@gmail.com>
2020-02-28 16:41:33 +0800

    add text classify service

--- python/examples/imdb/text_classify_service.py
+++ python/examples/imdb/text_classify_service.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #!flask/bin/python
-from plugin_service import PluginService
+from paddle_serving_client.plugin_service import PluginService
 import sys
 
 class IMDBService(PluginService):
