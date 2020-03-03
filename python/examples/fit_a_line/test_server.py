8e5afd01cb6b6938689f6db90dc49b7ad034b7d2
guru4elephant <guru4elephant@gmail.com>
2020-03-03 13:25:15 +0800

    update model and readme for fit a line

--- python/examples/fit_a_line/test_server.py
+++ python/examples/fit_a_line/test_server.py
@@ -7,13 +7,15 @@ from paddle_serving_server import Server
 op_maker = OpMaker()
 read_op = op_maker.create('general_reader')
 general_infer_op = op_maker.create('general_infer')
+response_op = op_maker.create('general_response')
 
 op_seq_maker = OpSeqMaker()
 op_seq_maker.add_op(read_op)
 op_seq_maker.add_op(general_infer_op)
+op_seq_maker.add_op(response_op)
 
 server = Server()
 server.set_op_sequence(op_seq_maker.get_op_sequence())
 server.load_model_config(sys.argv[1])
-server.prepare_server(workdir="work_dir1", port=9292, device="cpu")
+server.prepare_server(workdir="work_dir1", port=9393, device="cpu")
 server.run_server()
