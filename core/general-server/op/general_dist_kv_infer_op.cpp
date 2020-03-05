5ad093ea6f3cd05c7358cf2a521da89a9980222d
wangjiawei04 <wangjiawei04@baidu.com>
2020-03-06 00:24:34 +0800

    WIP: add general dist kv with op, and criteo kv demo

---
+++ core/general-server/op/general_dist_kv_infer_op.cpp
@@ -0,0 +1,128 @@
+// Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "core/general-server/op/general_dist_kv_infer_op.h"
+#include <algorithm>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include "core/predictor/framework/infer.h"
+#include "core/predictor/framework/memory.h"
+#include "core/predictor/framework/resource.h"
+#include "core/util/include/timer.h"
+#include "core/cube/cube-api/include/cube_api.h"
+
+namespace baidu {
+namespace paddle_serving {
+namespace serving {
+
+using baidu::paddle_serving::Timer;
+using baidu::paddle_serving::predictor::MempoolWrapper;
+using baidu::paddle_serving::predictor::general_model::Tensor;
+using baidu::paddle_serving::predictor::general_model::Response;
+using baidu::paddle_serving::predictor::general_model::Request;
+using baidu::paddle_serving::predictor::general_model::FetchInst;
+using baidu::paddle_serving::predictor::InferManager;
+using baidu::paddle_serving::predictor::PaddleGeneralModelConfig;
+
+int GeneralDistKVInferOp::inference() {
+  VLOG(2) << "Going to run inference";
+  const GeneralBlob *input_blob = get_depend_argument<GeneralBlob>(pre_name());
+  VLOG(2) << "Get precedent op name: " << pre_name();
+  GeneralBlob *output_blob = mutable_data<GeneralBlob>();
+
+  if (!input_blob) {
+    LOG(ERROR) << "Failed mutable depended argument, op:" << pre_name();
+    return -1;
+  }
+
+  const TensorVector *in = &input_blob->tensor_vector;
+  TensorVector *out = &output_blob->tensor_vector;
+  int batch_size = input_blob->GetBatchSize();
+  VLOG(2) << "input batch size: " << batch_size;
+  std::vector<uint64_t> keys;
+  std::vector<rec::mcube::CubeValue> values;
+  int sparse_count = 0;
+  for (size_t i = 0; i < in->size(); ++i) {
+    if (in->at(i).name == "dense_input") continue;
+    ++sparse_count;
+    size_t elem_num = 1;
+    for (size_t s = 0; s < in->at(i).shape.size(); ++s) {
+      elem_num *= in->at(i).shape[s];
+    }
+    std::cout << elem_num << std::endl;
+    int64_t* data_ptr = static_cast<int64_t*>(in->at(i).data.data());
+    for (size_t j = 0; j < elem_num; ++j) {
+      keys.push_back(data_ptr[j]);
+      std::cout << data_ptr[j] << " ";
+    }
+    std::cout << std::endl;
+  }  
+  //TODO: Add Seek CubeValues Here, and replace EMBEDDING_SIZE with variable.
+  rec::mcube::CubeAPI *cube = rec::mcube::CubeAPI::instance();
+  cube->init("./work_dir1/cube.conf");
+  std::string table_name = "test_dict";
+  int ret = cube->seek(table_name, keys, &values);
+  for (int vi = 0; vi < values.size(); ++vi) {
+    std::cout << "value idx: " << vi << " , value: " << values[vi].buff << std::endl;
+  }
+ 
+  size_t EMBEDDING_SIZE = 9; 
+  TensorVector dist_kv_out;
+  dist_kv_out.resize(sparse_count);
+  int cube_val_idx = 0;
+  int sparse_idx = 0;
+  for (size_t i = 0; i < dist_kv_out.size(); ++i) {
+    if  (in->at(i).name == "dense_input") continue;
+    std::copy(in->at(sparse_idx).lod.begin(), in->at(sparse_idx).lod.end(), dist_kv_out[sparse_idx].lod.begin());
+    std::cout << "sparse idx: " << sparse_idx << " , idx: " << i << std::endl;
+    ++sparse_idx;
+    dist_kv_out[i].dtype = paddle::PaddleDType::FLOAT32;
+    dist_kv_out[i].shape.push_back(dist_kv_out[i].lod[0].back());
+    dist_kv_out[i].shape.push_back(EMBEDDING_SIZE); 
+    dist_kv_out[i].name =  "embedding_"+ std::to_string(i)+ ".tmp_0";
+    dist_kv_out[i].data.Resize(dist_kv_out[i].lod[0].back() * EMBEDDING_SIZE * sizeof(float));
+    float *dst_ptr = static_cast<float*>(dist_kv_out[i].data.data());
+    for (int x = 0; x < dist_kv_out[i].lod[0].back(); ++x) {
+      float * data_ptr = dst_ptr +  x* EMBEDDING_SIZE;
+      std::cout << "x: " << x << std::endl;
+      memcpy(data_ptr, values[cube_val_idx].buff.data(), values[cube_val_idx].buff.size());
+      cube_val_idx++;
+    }
+  }  
+  
+  output_blob->SetBatchSize(batch_size);
+
+  VLOG(2) << "infer batch size: " << batch_size;
+
+  Timer timeline;
+  int64_t start = timeline.TimeStampUS();
+  timeline.Start();
+
+  if (InferManager::instance().infer(GENERAL_MODEL_NAME, &dist_kv_out, out, batch_size)) {
+    LOG(ERROR) << "Failed do infer in fluid model: " << GENERAL_MODEL_NAME;
+    return -1;
+  }
+
+  int64_t end = timeline.TimeStampUS();
+  CopyBlobInfo(input_blob, output_blob);
+  AddBlobInfo(output_blob, start);
+  AddBlobInfo(output_blob, end);
+  return 0;
+}
+DEFINE_OP(GeneralDistKVInferOp);
+
+}  // namespace serving
+}  // namespace paddle_serving
+}  // namespace baidu
---
+++ python/examples/criteo_ctr/test_client_kv.py
@@ -0,0 +1,30 @@
+from paddle_serving_client import Client
+import paddle
+import sys
+import os
+import criteo_py as criteo
+from paddle_serving_client.metric import auc
+
+client = Client()
+client.load_client_config(sys.argv[1])
+client.connect(["127.0.0.1:9292"])
+
+batch = 1
+buf_size = 100
+dataset = criteo.CriteoDataset()
+dataset.setup(1000001)
+test_filelists = ["{}/part-%d".format(sys.argv[2]) % x
+                 for x in range(len(os.listdir(sys.argv[2])))]
+reader = dataset.infer_reader(test_filelists[len(test_filelists)-40:], batch, buf_size)
+
+label_list = []
+prob_list = []
+for data in reader():
+    feed_dict = {}
+    feed_dict['dense_input'] = data[0][0]
+    for i in range(1, 27):
+        feed_dict["sparse_{}".format(i - 1)] = data[0][i]
+    print (feed_dict)
+    fetch_map = client.predict(feed=feed_dict, fetch=["prob"])
+    print(fetch_map)
+
