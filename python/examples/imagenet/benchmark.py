297341a2b15a20e6fed7beee13d4fa5ca97eeb08
MRXLT <xlt2024@gmail.com>
2020-03-13 17:16:55 +0800

    refine imagenet benchmark script

--- python/examples/imagenet/benchmark.py
+++ python/examples/imagenet/benchmark.py
@@ -18,23 +18,28 @@ from paddle_serving_client import Client
 from paddle_serving_client.utils import MultiThreadRunner
 from paddle_serving_client.utils import benchmark_args
 import time
+import os
 
 args = benchmark_args()
 
 
 def single_func(idx, resource):
+    file_list = []
+    for file_name in os.listdir("./image_data/n01440764"):
+        file_list.append(file_name)
+    img_list = []
+    for i in range(1000):
+        img_list.append(open("./image_data/n01440764/" + file_list[i]).read())
     if args.request == "rpc":
         reader = ImageReader()
         fetch = ["score"]
         client = Client()
         client.load_client_config(args.model)
-        client.connect([resource["endpoint"][idx % 4]])
+        client.connect([resource["endpoint"][idx % len(resource["endpoint"])]])
 
         start = time.time()
         for i in range(1000):
-            with open("./data/n01440764_10026.JPEG") as f:
-                img = f.read()
-            img = reader.process_image(img).reshape(-1)
+            img = reader.process_image(img_list[i]).reshape(-1)
             fetch_map = client.predict(feed={"image": img}, fetch=["score"])
         end = time.time()
         return [[end - start]]
@@ -43,10 +48,14 @@ def single_func(idx, resource):
 
 if __name__ == "__main__":
     multi_thread_runner = MultiThreadRunner()
-    endpoint_list = []
-    card_num = 4
-    for i in range(args.thread):
-        endpoint_list.append("127.0.0.1:{}".format(9295 + i % card_num))
+    endpoint_list = ["127.0.0.1:9393"]
+    #card_num = 4
+    #for i in range(args.thread):
+    #    endpoint_list.append("127.0.0.1:{}".format(9295 + i % card_num))
     result = multi_thread_runner.run(single_func, args.thread,
                                      {"endpoint": endpoint_list})
-    print(result)
+    avg_cost = 0
+    for i in range(args.thread):
+        avg_cost += result[0][i]
+    avg_cost = avg_cost / args.thread
+    print("average total cost {} s.".format(avg_cost))
