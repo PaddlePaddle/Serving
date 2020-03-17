fae76c6aacc458adec153ebdb779b9e6f356233a
wangjiawei04 <wangjiawei04@baidu.com>
2020-03-17 16:36:14 +0000

    mv criteo_ctr, to criteo_ctr_with_cube

--- python/examples/criteo_ctr/start.sh
+++
@@ -1,8 +0,0 @@
-sleep 5
-cd cube
-./cube-cli -config_file conf/cube.conf  -keys keys -dict test_dict -print_output true -batch 1
-cd ..
-mkdir work_dir1
-yes | cp cube/conf/cube.conf ./work_dir1/
-python test_server.py ctr_serving_model_kv &
-python test_client.py ctr_client_conf/serving_client_conf.prototxt ./raw_data &
