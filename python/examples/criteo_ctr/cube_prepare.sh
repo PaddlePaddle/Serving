fe56060f2eb98034a8ac490541091058a9ecbd4a
wangjiawei04 <wangjiawei04@baidu.com>
2020-03-17 08:41:28 +0000

    add cube_local

---
+++ python/examples/criteo_ctr/cube_prepare.sh
@@ -0,0 +1,8 @@
+
+mkdir -p cube_model
+mkdir -p cube/data
+./dump_parameter ctr_serving_model/SparseFeatFactors ./cube_model/feature
+/Serving/python/examples/criteo_ctr/cube/cube-builder -dict_name=test_dict -job_mode=base -last_version=0 -cur_version=0 -depend_version=0 -input_path=/Serving/python/examples/criteo_ctr/cube_model -output_path=/Serving/python/examples/criteo_ctr/cube/data -shard_num=1  -only_build=false
+mv /Serving/python/examples/criteo_ctr/cube/data/0_0/test_dict_part0/* /Serving/python/examples/criteo_ctr/cube/data/
+cd cube && ./cube 
+
