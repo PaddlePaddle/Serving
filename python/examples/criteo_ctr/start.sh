1d2b3f63613dbe07590afc92997f47c224c97c22
wangjiawei04 <wangjiawei04@baidu.com>
2020-03-17 09:54:31 +0000

    fix start.sh

--- python/examples/criteo_ctr/start.sh
+++ python/examples/criteo_ctr/start.sh
@@ -1,15 +1,4 @@
-cd fileserver
-nohup python -m SimpleHTTPServer 8080 > fs.log &
-cd ..
-cd cube/
-nohup ./cube > nohup_cube.log &
-nohup ./cube-agent -P 8001 > nohup_agent.log &
-sleep 1
-nohup ./cube-transfer -p 8099 -l 4 --config conf/transfer.conf > nohup_transfer.log &
-cd /
-nohup python -m SimpleHTTPServer 8098 > fs.log &
-cd -
-sleep 30
+sleep 5
 cd cube
 ./cube-cli -config_file conf/cube.conf  -keys keys -dict test_dict -print_output true -batch 1
 cd ..
