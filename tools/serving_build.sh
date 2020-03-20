fba899a6cba5b1f24483c8b5d789d98a0556c4f4
wangjiawei04 <wangjiawei04@baidu.com>
2020-03-20 08:13:27 +0000

    fix ci script 2 test=serving

--- tools/serving_build.sh
+++ tools/serving_build.sh
@@ -113,6 +113,9 @@ function python_run_criteo_ctr_with_cube() {
     check_cmd "mv models/data ./cube/"
     check_cmd "mv models/ut_data ./"
     cp ../../../build-server-$TYPE/output/bin/cube* ./cube/ 
+    mkdir -p $PYTHONROOT/lib/python2.7/site-packages/paddle_serving_server/serving-cpu-avx-openblas-0.1.3/
+    yes | cp ../../../build-server-$TYPE/output/demo/serving/bin/serving $PYTHONROOT/lib/python2.7/site-packages/paddle_serving_server/serving-cpu-avx-openblas-0.1.3/
+
     sh cube_prepare.sh &
     check_cmd "mkdir work_dir1 && cp cube/conf/cube.conf ./work_dir1/"    
     python test_server.py ctr_serving_model_kv &
