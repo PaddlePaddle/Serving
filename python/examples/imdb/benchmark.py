75721dfa6bbc88a138db114c393540a86b808f8b
barrierye <barriery@qq.com>
2020-03-23 20:12:11 +0800

    recover client.connect() function && test=serving

--- python/examples/imdb/benchmark.py
+++ python/examples/imdb/benchmark.py
@@ -35,8 +35,7 @@ def single_func(idx, resource):
     if args.request == "rpc":
         client = Client()
         client.load_client_config(args.model)
-        client.add_variant("var1", [args.endpoint], 50)
-        client.connect()
+        client.connect([args.endpoint])
         for i in range(1000):
             if args.batch_size == 1:
                 word_ids, label = imdb_dataset.get_words_and_label(line)
