de2b3b06645936cdfca84696d9218c57353b0e9e
barrierye <barriery@qq.com>
2020-03-25 21:27:51 +0800

    print info before calculate auc in cube test && test=serving

--- tools/serving_build.sh
+++ tools/serving_build.sh
@@ -193,7 +193,7 @@ function python_run_criteo_ctr_with_cube() {
             check_cmd "mkdir work_dir1 && cp cube/conf/cube.conf ./work_dir1/"    
             python test_server.py ctr_serving_model_kv &
             check_cmd "python test_client.py ctr_client_conf/serving_client_conf.prototxt ./ut_data >score"
-            tail -n 2 score
+            tail -n 2 score # for show why auc < 0.7
             AUC=$(tail -n 2  score | awk 'NR==1')
             VAR2="0.70"
             RES=$( echo "$AUC>$VAR2" | bc )
