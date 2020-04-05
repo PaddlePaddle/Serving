ce7e742246ebd11cf90ae7b9590583ff8707d4cf
MRXLT <xlt2024@gmail.com>
2020-04-05 05:42:13 +0000

    fix cube ut

--- tools/serving_build.sh
+++ tools/serving_build.sh
@@ -191,6 +191,7 @@ function python_run_criteo_ctr_with_cube() {
             sh cube_prepare.sh &
             check_cmd "mkdir work_dir1 && cp cube/conf/cube.conf ./work_dir1/"    
             python test_server.py ctr_serving_model_kv &
+            sleep 5
             check_cmd "python test_client.py ctr_client_conf/serving_client_conf.prototxt ./ut_data >score"
             tail -n 2 score
             AUC=$(tail -n 2  score | awk 'NR==1')
@@ -217,6 +218,7 @@ function python_run_criteo_ctr_with_cube() {
             sh cube_prepare.sh &
             check_cmd "mkdir work_dir1 && cp cube/conf/cube.conf ./work_dir1/"
             python test_server_gpu.py ctr_serving_model_kv &
+            sleep 5
             check_cmd "python test_client.py ctr_client_conf/serving_client_conf.prototxt ./ut_data >score"
             tail -n 2 score | awk 'NR==1'
             AUC=$(tail -n 2  score | awk 'NR==1')
