8f820382b9c0694befa25e195dbf2572a7e5c588
MRXLT <xlt2024@gmail.com>
2020-04-26 15:46:46 +0800

    add readme

--- python/examples/senta/get_data.sh
+++ python/examples/senta/get_data.sh
@@ -1,5 +1,5 @@
 wget https://paddle-serving.bj.bcebos.com/paddle_hub_models/text/SentimentAnalysis/senta_bilstm.tar.gz --no-check-certificate
 tar -xzvf senta_bilstm.tar.gz
 
-wget https://baidu-nlp.bj.bcebos.com/sentiment_classification-dataset-1.0.0.tar.gz
+wget https://baidu-nlp.bj.bcebos.com/sentiment_classification-dataset-1.0.0.tar.gz --no-check-certificate
 tar -zxvf sentiment_classification-dataset-1.0.0.tar.gz
