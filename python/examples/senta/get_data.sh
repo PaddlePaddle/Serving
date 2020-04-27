2bcce4d5385355a8a2d94f31ad6577e2da2ea59c
MRXLT <xlt2024@gmail.com>
2020-04-27 13:54:28 +0800

    add download link

--- python/examples/senta/get_data.sh
+++ python/examples/senta/get_data.sh
@@ -1,5 +1,7 @@
-wget https://paddle-serving.bj.bcebos.com/paddle_hub_models/text/SentimentAnalysis/senta_bilstm.tar.gz --no-check-certificate
-tar -xzvf senta_bilstm.tar.gz
-
-wget https://baidu-nlp.bj.bcebos.com/sentiment_classification-dataset-1.0.0.tar.gz --no-check-certificate
-tar -zxvf sentiment_classification-dataset-1.0.0.tar.gz
+#wget https://paddle-serving.bj.bcebos.com/paddle_hub_models/text/SentimentAnalysis/senta_bilstm.tar.gz --no-check-certificate
+#tar -xzvf senta_bilstm.tar.gz
+wget https://paddle-serving.bj.bcebos.com/paddle_hub_models/text/LexicalAnalysis/lac_model.tar.gz --no-check-certificate
+tar -xzvf lac_model.tar.gz
+wget https://paddle-serving.bj.bcebos.com/reader/lac/lac_dict.tar.gz  --no-check-certificate
+tar -xzvf lac_dict.tar.gz
+wget https://paddle-serving.bj.bcebos.com/reader/senta/vocab.txt --no-check-certificate
