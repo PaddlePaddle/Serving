d6e8cf19d756cc650d3d30b3fd12c4f075b15cfa
barrierye <barriery@qq.com>
2020-05-22 15:17:50 +0800

    add PyClient

--- python/examples/imdb/test_py_client.py
+++ python/examples/imdb/test_py_client.py
@@ -11,41 +11,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# pylint: disable=doc-string-missing
-import grpc
-import general_python_service_pb2
-import general_python_service_pb2_grpc
+from pyclient import PyClient
 import numpy as np
 
-channel = grpc.insecure_channel('localhost:8080')
-stub = general_python_service_pb2_grpc.GeneralPythonServiceStub(channel)
-req = general_python_service_pb2.Request()
-"""
-# line = "i am very sad | 0"
-word_ids = np.array([8, 233, 52, 601], dtype='int64')
-# word_ids = np.array([8, 233, 52, 601])
-print(word_ids)
-data = np.ndarray.tobytes(word_ids)
-print(data)
-# xx = np.frombuffer(data)
-xx = np.frombuffer(data, dtype='int64')
-print (xx)
-req.feed_var_names.append("words")
-req.feed_insts.append(data)
-"""
+client = PyClient()
+client.connect('localhost:8080')
+
 x = np.array(
     [
         0.0137, -0.1136, 0.2553, -0.0692, 0.0582, -0.0727, -0.1583, -0.0584,
         0.6283, 0.4919, 0.1856, 0.0795, -0.0332
     ],
     dtype='float')
-data = np.ndarray.tobytes(x)
-req.feed_var_names.append("x")
-req.feed_insts.append(data)
 
-for i in range(100):
-    resp = stub.inference(req)
-    for idx, name in enumerate(resp.fetch_var_names):
-        print('{}: {}'.format(
-            name, np.frombuffer(
-                resp.fetch_insts[idx], dtype='float')))
+for i in range(5):
+    fetch_map = client.predict(
+        feed={"x": x}, fetch_with_type={"combine_op_output": "float"})
+    print(fetch_map)
