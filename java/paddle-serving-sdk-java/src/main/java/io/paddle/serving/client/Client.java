add04dac362ba1ffd791b7ce9dc7e15a060d929d
barrierye <barriery@qq.com>
2020-06-22 19:31:45 +0800

    update code

--- java/paddle-serving-sdk-java/src/main/java/io/paddle/serving/client/Client.java
+++ java/paddle-serving-sdk-java/src/main/java/io/paddle/serving/client/Client.java
@@ -1,9 +1,14 @@
 package io.paddle.serving.client;
 
+import java.util.*;
+
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
+//import com.google.protobuf;
+
 import io.paddle.serving.grpc.*;
+import io.paddle.serving.configure.*;
 
 
 public class Client {
@@ -11,6 +16,13 @@ public class Client {
     private MultiLangGeneralModelServiceGrpc.MultiLangGeneralModelServiceBlockingStub blockingStub_;
     private MultiLangGeneralModelServiceGrpc.MultiLangGeneralModelServiceStub asyncStub_;
     private double rpcTimeoutS_;
+    private List<String> feedNames_;
+    private Map<String, Integer> feedTypes_;
+    private Map<String, List<Integer>> feedShapes_;
+    private List<String> fetchNames_;
+    private Map<String, Integer> fetchTypes_;
+    private Map<String, List<Integer>> fetchShapes_;
+    private Set<String> lodTensorSet_;
 
     Client() {
         channel_ = null;
@@ -65,7 +77,35 @@ public class Client {
     }
 
     void _parseModelConfig(String model_config_str) {
+        GeneralModelConfig.Builder model_conf_builder = GeneralModelConfig.newBuilder();
+        try {
+            com.google.protobuf.TextFormat.getParser().merge(model_config_str, model_conf_builder);
+        } catch (com.google.protobuf.TextFormat.ParseException e) {
+            System.out.format("Parse client config failed: %s", e.toString());
+        }
+        GeneralModelConfig model_conf = model_conf_builder.build();
 
+        List<String> feedNames_ ;
+        List<FeedVar> feed_var_list = model_conf.getFeedVarList();
+        for (FeedVar feed_var : feed_var_list) {
+            feedNames_.add(feed_var.getAliasName());
+        }
+        List<String> fetchNames_ ;
+        List<FetchVar> fetch_var_list = model_conf.getFetchVarList();
+        for (FetchVar fetch_var : fetch_var_list) {
+            fetchNames_.add(fetch_var.getAliasName());
+        }
+        feedTypes_ = new HashMap<String, Integer>();
+        feedShapes_ = new HashMap<String, List<Integer>>();
+        fetchTypes_ = new HashMap<String, Integer>();
+        fetchShapes_ = new HashMap<String, List<Integer>>();
+        lodTensorSet_ = new HashSet<String>();
+
+        for (int i = 0; i < feed_var_list.size(); ++i) {
+            FeedVar feed_var = feed_var_list[i];
+            String var_name = feed_var.getAliasName();
+            // feedTypes_[var_name] = feed_var.getFeedType();
+        }
     }
 
     public static void main( String[] args ) {
