1f1a433fa7fab26d42849e5ef8b26bc116881b54
dongdaxiang <dongdaxiang@baidu.com>
2020-06-23 21:20:59 +0800

    refactor pipeline server

--- python/pipeline/pipeline_server.py
+++ python/pipeline/pipeline_server.py
@@ -12,3 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # pylint: disable=doc-string-missing
+
+
+class PipelineService(pipeline_service_pb2_grpc.PipelineServiceServicer):
+    def __init__(self, in_channel, out_channel, retry=2):
+        super(PipelineService, self).__init__()
+        pass
+
+
+class PipelineServer(object):
+    def __init__(self):
+        pass
+
+    def set_response_op(self, response_op):
+        pass
+
+    def prepare_server(self, yml_file):
+        pass
+
+    def run_server(self):
+        pass
