322ba44641267cec539ec4eb89461853ce997e9c
barrierye <barriery@qq.com>
2020-07-14 12:00:31 +0800

    update code structure

---
+++ java/examples/src/main/java/PaddleServingClientExample.java
@@ -0,0 +1,11 @@
+import io.paddle.serving.client.Client;
+/**
+ * Hello world!
+ *
+ */
+public class PaddleServingClientExample {
+    public static void main( String[] args ) {
+        Client client = new Client();
+        System.out.println( "Hello World!" );
+    }
+}
