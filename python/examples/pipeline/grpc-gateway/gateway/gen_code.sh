12223136fa36edfe70be242ad646693fc97d3c97
barriery <barriery@qq.com>
2020-08-18 03:23:36 +0000

    add code for gateway

---
+++ python/examples/pipeline/grpc-gateway/gateway/gen_code.sh
@@ -0,0 +1,13 @@
+# generate <path/to/your_service>.pb.go
+protoc -I/usr/local/include -I. \
+    -I$GOPATH/src \
+    -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    --go_out=plugins=grpc:. \
+    gateway.proto
+
+# generate <path/to/your_service>.gw.go
+protoc -I/usr/local/include -I. \
+    -I$GOPATH/src \
+    -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    --grpc-gateway_out=logtostderr=true:. \
+    gateway.proto
