80f110329a8d8b2aa20009c48a290352507b4bfb
barriery <barriery@qq.com>
2020-08-20 05:52:18 +0000

    fix ci script

--- tools/serving_build.sh
+++ tools/serving_build.sh
@@ -25,6 +25,7 @@ function init() {
     go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
     go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
     go get -u github.com/golang/protobuf/protoc-gen-go
+    go get -u golang.org/x/net/context
 }
 
 function check_cmd() {
