eb6c329ebb26631aaa5eb7151f1a55530b67dd96
barriery <barriery@qq.com>
2020-08-21 10:06:55 +0000

    fix compile doc

--- doc/COMPILE.md
+++ doc/COMPILE.md
@@ -75,7 +75,7 @@ export PATH=$PATH:$GOPATH/bin
 go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
 go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
 go get -u github.com/golang/protobuf/protoc-gen-go
-go get -u golang.org/x/net/context
+go get -u google.golang.org/grpc
 ```
 
 ## Compile Server
--- tools/serving_build.sh
+++ tools/serving_build.sh
@@ -25,7 +25,7 @@ function init() {
     go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
     go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
     go get -u github.com/golang/protobuf/protoc-gen-go
-    go get -u golang.org/x/net/context
+    go get -u google.golang.org/grpc
 }
 
 function check_cmd() {
