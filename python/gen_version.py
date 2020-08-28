ea03b14352696e85b0014343d48ba7f2d9524838
MRXLT <xlt2024@gmail.com>
2020-08-28 16:34:20 +0800

    fix compile

--- python/gen_version.py
+++ python/gen_version.py
@@ -23,7 +23,7 @@ def update_info(file_name, feature, info):
     with open(file_name, "r") as f:
         for line in f.readlines():
             if re.match(feature, line):
-                line = feature + " = \"" + info.strip().decode("utf-8") + "\"\n"
+                line = feature + " = \"" + info.decode('utf-8').strip() + "\"\n"
             new_str = new_str + line
 
     with open(file_name, "w") as f:
