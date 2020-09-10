7ca0245f911c83d3a53a2aa9438a18fa51e84032
MRXLT <xlt2024@gmail.com>
2020-09-10 09:30:36 +0000

    bug fix

--- python/paddle_serving_app/reader/__init__.py
+++ python/paddle_serving_app/reader/__init__.py
@@ -14,7 +14,7 @@
 from .chinese_bert_reader import ChineseBertReader
 from .image_reader import ImageReader, File2Image, URL2Image, Sequential, Normalize, Base64ToImage
 from .image_reader import CenterCrop, Resize, Transpose, Div, RGB2BGR, BGR2RGB, ResizeByFactor
-from .image_reader import RCNNPostprocess, SegPostprocess, PadStride
+from .image_reader import RCNNPostprocess, SegPostprocess, PadStride, BlazeFacePostprocess
 from .image_reader import DBPostProcess, FilterBoxes, GetRotateCropImage, SortedBoxes
 from .lac_reader import LACReader
 from .senta_reader import SentaReader
