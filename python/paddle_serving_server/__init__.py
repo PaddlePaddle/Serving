21a615307d35f27629ca7b9ba73648f229983e48
MRXLT <xlt2024@gmail.com>
2020-10-27 19:33:24 +0800

    fix server for python3

--- python/paddle_serving_server/__init__.py
+++ python/paddle_serving_server/__init__.py
@@ -584,7 +584,7 @@ class MultiLangServerServiceServicer(multi_lang_general_model_service_pb2_grpc.
                     else:
                         raise Exception("error type.")
                 tensor.shape.extend(list(model_result[name].shape))
-                if model_result.has_key("{}.lod".format(name)):
+                if "{}.lod".format(name) in model_result:
                     tensor.lod.extend(model_result["{}.lod".format(name)]
                                       .tolist())
                 inst.tensor_array.append(tensor)
