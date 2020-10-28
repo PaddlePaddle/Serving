da8903d5dc35e8dd48706606ac0356a188585ffa
MRXLT <xlt2024@gmail.com>
2020-10-28 14:26:46 +0800

    fix ut

--- python/examples/grpc_impl_example/fit_a_line/test_timeout_client.py
+++ python/examples/grpc_impl_example/fit_a_line/test_timeout_client.py
@@ -28,8 +28,7 @@ for i in range(3):
     fetch_map = client.predict(feed={"x": x}, fetch=["price"])
     if fetch_map["serving_status_code"] == 0:
         print(fetch_map)
-    elif fetch_map["serving_status_code"].code(
-    ) == grpc.StatusCode.DEADLINE_EXCEEDED:
+    elif fetch_map["serving_status_code"] == grpc.StatusCode.DEADLINE_EXCEEDED:
         print('timeout')
     else:
         print(fetch_map["serving_status_code"])
