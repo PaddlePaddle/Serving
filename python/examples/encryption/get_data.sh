1de35dfeccf686b4a68456890afa3984355fab17
Jiawei Wang <wangjiawei04@baidu.com>
2021-01-22 19:10:47 +0800

    Merge pull request #988 from HexToString/fix_encryption_getdata.sh
    fix encryption get_data.sh bug

--- python/examples/encryption/get_data.sh
+++ python/examples/encryption/get_data.sh
@@ -1,4 +1,4 @@
 wget --no-check-certificate https://paddle-serving.bj.bcebos.com/uci_housing_example/encrypt.tar.gz
 tar -xzf encrypt.tar.gz
-cp -rvf ../fit_a_line/uci_housing_model .
-cp -rvf ../fit_a_line/uci_housing_client .
+wget --no-check-certificate https://paddle-serving.bj.bcebos.com/uci_housing.tar.gz
+tar -xzf uci_housing.tar.gz
