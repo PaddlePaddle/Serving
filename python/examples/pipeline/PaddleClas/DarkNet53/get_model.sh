bb5c8da5480ac237a9e36397953f90c239a3f7e4
LDOUBLEV <liuvv0203@outlook.com>
2021-05-17 07:35:45 +0000

    add 11 models to paddleclas

---
+++ python/examples/pipeline/PaddleClas/DarkNet53/get_model.sh
@@ -0,0 +1,5 @@
+wget --no-check-certificate https://paddle-serving.bj.bcebos.com/model/DarkNet53.tar
+tar -xf DarkNet53.tar
+
+wget --no-check-certificate https://paddle-serving.bj.bcebos.com/imagenet-example/image_data.tar.gz
+tar -xzvf image_data.tar.gz
---
+++ python/examples/pipeline/PaddleClas/MobileNetV2/get_model.sh
@@ -0,0 +1,5 @@
+wget --no-check-certificate https://paddle-serving.bj.bcebos.com/model/MobileNetV2.tar
+tar -xf MobileNetV2.tar
+
+wget --no-check-certificate https://paddle-serving.bj.bcebos.com/imagenet-example/image_data.tar.gz
+tar -xzvf image_data.tar.gz
