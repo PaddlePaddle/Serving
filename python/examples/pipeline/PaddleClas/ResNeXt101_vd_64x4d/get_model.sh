8f7f489b1212cd502536399f87b5dcfac58ff3d4
Jiawei Wang <wangjiawei04@baidu.com>
2021-05-18 23:22:10 +0800

    Merge pull request #1245 from bjjwwang/develop
    Fix Clas sh get_model.sh

--- python/examples/pipeline/PaddleClas/ResNeXt101_vd_64x4d/get_model.sh
+++ python/examples/pipeline/PaddleClas/ResNeXt101_vd_64x4d/get_model.sh
@@ -1,4 +1,4 @@
-wget --no-check-certificate https://paddle-serving.bj.bcebos.com/ResNeXt101_vd_64x4d.tar
+wget --no-check-certificate https://paddle-serving.bj.bcebos.com/model/ResNeXt101_vd_64x4d.tar
 tar -xf ResNeXt101_vd_64x4d.tar
 
 wget --no-check-certificate https://paddle-serving.bj.bcebos.com/imagenet-example/image_data.tar.gz
