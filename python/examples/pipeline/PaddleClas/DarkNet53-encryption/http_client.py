836ccfe54744d991e8bb842affd68ac37cc2532f
Jiawei Wang <wangjiawei04@baidu.com>
2021-08-17 10:17:17 +0000

    add DarkNet53-encryption

---
+++ python/examples/pipeline/PaddleClas/DarkNet53-encryption/http_client.py
@@ -0,0 +1,20 @@
+import numpy as np
+import requests
+import json
+import cv2
+import base64
+import os
+
+def cv2_to_base64(image):
+    return base64.b64encode(image).decode('utf8')
+
+if __name__ == "__main__":
+    url = "http://127.0.0.1:18080/imagenet/prediction"
+    with open(os.path.join(".", "daisy.jpg"), 'rb') as file:
+        image_data1 = file.read()
+    image = cv2_to_base64(image_data1)
+    header = {"Content-Type":"application/json", "apikey":"WeJn7tVjuujtGxBgl6cWRGpmL2VMEBdb", "X-INSTANCE-ID" : "kong_ins10"}
+    data = {"key": ["image"], "value": [image]}
+    for i in range(1):
+        r = requests.post(url=url, data=json.dumps(data))
+        print(r.json())
