a1e33e469fca1fea97f777ef9229abef7402dce7
HexToString <506181616@qq.com>
2022-01-24 20:20:43 +0800

    fix async while

--- core/predictor/framework/resource.h
+++ core/predictor/framework/resource.h
@@ -24,6 +24,7 @@
 #include "core/predictor/common/inner_common.h"
 #include "core/predictor/framework/infer.h"
 #include "core/predictor/framework/memory.h"
+#include "core/predictor/framework/thread_mutex.h"
 
 namespace baidu {
 namespace paddle_serving {
